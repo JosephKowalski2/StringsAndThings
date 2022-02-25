42ec29634d97941532aa65d8a9bf6608dfa24c03
Joseph Kowalski <jkjr25@gmail.com>
2022-02-25 11:43:14 -0500

    Completed removeString

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -41,7 +41,7 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+        return base.replaceAll(remove, "");
     }
 
     /**
